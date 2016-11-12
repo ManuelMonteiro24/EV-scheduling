f27683027389da092eade4e81b1ebbb197561900
ManuelMonteiro24 <tic-manuel13b@hotmail.com>
2016-11-12 00:45:17 +0000

    leonor post 1
    updates na global_solution que se tem  que passar para a global
    solution con a lifebaterry time . ja com algumas simulações que esta
    nos comentários

--- global_solution.m
+++ global_solution.m
@@ -1,32 +1,37 @@
-%information variables
+%% Optimal Scheduling for Charging and Discharging of Electric Vehicles
+%
+% Authors: Manuel Monteiro, Leonor Fermoselle, Miguel Paulino, Inês
+% Peixoto
+%
+% Date: xx/12/2016
+%%
+%Variable initialization
 
-%Interval set (24 intervalos)
-N = 24;
+load('EV_data.mat');
 
-%EV's set (200 carros)
-M = 200;
-% others sets ???? 
-M_V2G = 0;
-M_CHG = 200;
+N = 24;         %Interval set
+M = 200;        %EV's set
+M_V2G = 0;      %Vehicle-to-grid set
+M_CHG = 200;    %Charging-only EV set
 
 %Electricity price model parameters (obtidos na seccao simulation settings)
-k0 = 10^-4; % unidades: C$/kWh
-k1 = 1.2*(10^-4); %unidades: C$/kWh/kW
+k0 = 10^-4; % units: [C$/kWh]
+k1 = 1.2*(10^-4); %units: [C$/kWh/kW]
 
 %Length of an interval (obtido no sistem model da solucao global)
-tau = 1; %unidades : 1 hora
+tau = 1; %units: [hour]
 
-%Battery capacity (obtida na seccao simulation settings)
-bat_cap = 16; %unidades : 16kWh 
+%Battery capacity (obtida na seccao simulation settings )
+bat_cap = 16; %units: [kWh] 
 
 %Maximum charging power (obtido na seccao simulation settings)
-pmax = 5; %unidades: 5kW
+pmax = 5; %units: [kW]
 
 %Final energy ratio required ???? confirmar????? pelo menos para as
 %experiencias eles consideram isso, (obtido na seccao simulation settings)
 fe_ratio = 0.9; 
 
-%matriz f
+%defining matrix f (charging-interval matrix)
 f = zeros(M,N);
 
 for m = 1:M
@@ -35,20 +40,12 @@ for m = 1:M
     end;
 end;   
 
+%% Part I - Global Scheduling solution
 %Solve the optimization problem
 
 cvx_begin quiet
 
-variables x(M,N) z(N);
-
-%Charging load at interval i
-for i = 1:N
-    var = 0;
-    for m = 1:M
-        var = (((x(m,i))*(f(m,i))) + var);
-    end;
-    z(i) == L_b(i) + var; %var = y(i)
-end;
+variables x(M,N) z(N) y(N) E(M,N);
 
 %Cost function
 f_cost = (k0*z(1)+(k1/2)*(z(1)^2)) - (k0*L_b(1)+(k1/2)*(L_b(1)^2));
@@ -63,25 +60,25 @@ minimize(f_cost);
 %Constrain that secures that the total load in an interval
 %is equal to the base load in that interval plus the sum of
 %the loads of all the cars plugged to a station in that interval
-
 for i = 1:N
     var = 0;
     for m = 1:M
         var = (x(m,i)*f(m,i)) + var;
     end;
+    y(i)== var;
     z(i) == L_b(i) + var;
 end;
 
 %Constrain that secures that the energy of a vehicle at the end of an
 %interval is no less than zero or greater than the battery capacity
 %confirmar esta constrain????
-
 for m = 1:M
     for i = 1:N %confirmar a ordem destes 2 primeiros for. e indiferente???
         var = 0;
         for k = EV_info(m,1):i %confirmar isto??
             var = ((x(m,k)*f(m,k))*tau) + var;   
         end;
+        E(m,i) == EV_info(m,3)+ var;
         EV_info(m,3)+ var <= bat_cap;
         EV_info(m,3)+ var >= 0;
     end;
@@ -89,7 +86,6 @@ end;
 
 %Constrain that secures that the final energy of a vehicle is no less
 %than the speficied energy level
-
 for m = 1:M
     var = 0;
     for i = 1:N
@@ -99,7 +95,7 @@ for m = 1:M
 end; 
 
 %Constrain that secures that the charging power of a charging only vehicle
-%isnt lower than zero or higher that the maximum charging power
+%isn't lower than zero or higher that the maximum charging power
 if M_CHG > 0 
     for m = 1:M_CHG
         for i = 1:N 
@@ -108,8 +104,9 @@ if M_CHG > 0
         end;
     end;
 end;
+
 %Constrain that secures that the charging power of a vehicle to grig type of
-%EV isnt lower than the minimun charging power or higher than the maximum 
+%EV isn't lower than the minimun charging power or higher than the maximum 
 %charging power
 if M_V2G > 0 
     for m = 1:M_V2G
@@ -119,9 +116,10 @@ if M_V2G > 0
         end;
     end;
 end;
+
 cvx_end;
 
-%simulation
+%% Part I: Simulations
 
 %sum of all x(m,i) in one interval
 for i = 1:N
@@ -138,3 +136,46 @@ figure(1); clf;
  title('Xi');
  subplot(1,2,2); stem(z,'LineWidth',5);
  title('zi');
+
+ %Charging load and Total load simulation
+figure(2);
+ stairs(y);
+ axis([0 24 -200 400]);
+ ylabel('Charging load [kW]');
+ xlabel('Time [h]');
+ 
+figure(3);
+ stairs(z);
+ axis([0 24 800 1600]);
+ ylabel('Total load [kW]');
+ xlabel('Time [h]');
+ 
+ %Energy and Charging power of EV 19
+ 
+figure(4);
+ stairs(E(19,:));
+ axis([0 24 0 15]);
+ ylabel('Energy of EV 19 [kWh]');
+ xlabel('Time [h]');
+ 
+%figure(5);
+ %bar(x(19));
+ %axis([0 24 -2 6]);
+ %ylabel('Charging power of EV 19 [kW]');
+ %xlabel('Time [h]');
+ 
+ %Charging only ratio simulation
+%figure(6);
+ %
+ %axis([0 1 230 270]);
+ %ylabel('Total cost [C$]');
+ %xlabel('Charging-only ratio');
+ 
+ 
+ %% Part II - ???
+ 
+ 
+ 
+ 
+ 
+ 
